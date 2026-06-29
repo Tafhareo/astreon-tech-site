d82bf63d1153c1c11f04d0ea64b6d09f19d87ee8
Tafhareo Estrela <tafhareoti@gmail.com>
2026-06-29 12:32:14 -0300

    feat: adicionar formulario do produto na home

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HomeFinalCTA from "@/components/home/HomeFinalCTA";
 import HomeHero from "@/components/home/HomeHero";
 import HomeImplementationProcess from "@/components/home/HomeImplementationProcess";
 import HomeOmnichannel from "@/components/home/HomeOmnichannel";
+import HomeProductLeadForm from "@/components/home/HomeProductLeadForm";
 import HomeSolutions from "@/components/home/HomeSolutions";
 import HomeTestimonials from "@/components/home/HomeTestimonials";
 import HomeWhyAstreon from "@/components/home/HomeWhyAstreon";
@@ -13,6 +14,7 @@ export default function Home() {
       <HomeHero />
       <HomeSolutions />
       <HomeOmnichannel />
+      <HomeProductLeadForm />
       <HomeWhyAstreon />
       <StatsSection />
       <HomeImplementationProcess />
